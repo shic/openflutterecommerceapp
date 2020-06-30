ac10f55e27a82fc9b4055494154d0cfe909206e4
shi <itshilei@gmail.com>
2020-06-30 02:48:29 +0200

    cart

--- lib/data/repositories/cart_repository_impl.dart
+++ lib/data/repositories/cart_repository_impl.dart
@@ -4,15 +4,19 @@
  * @see cart_repository_impl.dart
  */
 
-import 'package:openflutterecommerce/data/repositories/abstract/cart_repository.dart';
+import 'dart:collection';
+
 import 'package:openflutterecommerce/data/model/cart_item.dart';
+import 'package:openflutterecommerce/data/model/commerce_image.dart';
 import 'package:openflutterecommerce/data/model/product.dart';
 import 'package:openflutterecommerce/data/model/product_attribute.dart';
 import 'package:openflutterecommerce/data/model/promo.dart';
+import 'package:openflutterecommerce/data/repositories/abstract/cart_repository.dart';
 
 class CartRepositoryImpl extends CartRepository {
   static CartProductDataStorage cartProductDataStorage =
       CartProductDataStorage();
+
   @override
   Future addProductToCart(Product product, int quantity,
       Map<ProductAttribute, String> selectedAttributes) {
@@ -49,5 +53,57 @@ class CartRepositoryImpl extends CartRepository {
 }
 
 class CartProductDataStorage {
-  List<CartItem> items = [];
+  static const sizeAttribute =
+      ProductAttribute(name: 'size', options: ['S', 'M', 'L', 'XL']);
+  static const colorAttribute =
+      ProductAttribute(name: 'color', options: ['grey', 'red', 'yellow']);
+
+  List<CartItem> items = [
+    CartItem(
+        product: Product(
+          1,
+          images: [
+            CommerceImage(
+              0,
+              'assets/images/products/laroxyl.jpeg',
+              '',
+              isLocal: true,
+            )
+          ],
+          discountPercent: 20,
+          averageRating: 5,
+          ratingCount: 10,
+          price: 15,
+          title: 'laroxyl',
+          selectableAttributes: [sizeAttribute, colorAttribute],
+          categories: [],
+          hashTags: [],
+          subTitle: null,
+        ),
+        selectedAttributes: HashMap.fromEntries([]),
+        productQuantity: ProductQuantity(1)),
+    CartItem(
+        product: Product(
+          2,
+          images: [
+            CommerceImage(
+              0,
+              'assets/images/products/tachipirina.jpeg',
+              '',
+              isLocal: true,
+            )
+          ],
+          discountPercent: 20,
+          averageRating: 5,
+          ratingCount: 10,
+          price: 15,
+          title: 'Tachipirina 1000mg',
+          selectableAttributes: [sizeAttribute, colorAttribute],
+          categories: [],
+          hashTags: [],
+          subTitle: null,
+        ),
+        selectedAttributes: HashMap.fromEntries([]),
+        productQuantity: ProductQuantity(1)),
+  ];
 }
